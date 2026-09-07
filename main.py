3fa65b5d23a6e06feaa94e2dec12dd0e67157122
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-09-07 16:33:55 +0300

    init
    start app task manger

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+    Точка входа в приложение Task Manager
+    version 0.0.1
+    --- description ---
+    -[x] создать репозиторий проекта
+    -[x] реализовать цикл приложения
+    -[ ] реализовать хранилище задач
+"""
+is_running = True
+collection = ['task1', 'task2', 'task3'] # list
+
+print("Добро пожаловать!")
+while is_running:
+    print(" старт ")
+    print(collection)
+    is_running = False
+    print(" финиш ")
+
+
+
+
+
+
+
