238e561d32d8a854e558559b683b255f91cd8543
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-09-13 19:20:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
+""" Приложение Task Manager
+    version 0.0.3
+    создает  i/o функционал для ввода заметки
 """
-    Точка входа в приложение Task Manager
-    version 0.0.1
-    --- description ---
-    -[x] создать репозиторий проекта
-    -[x] реализовать цикл приложения
-    -[ ] реализовать хранилище задач
-"""
+
 is_running = True
-collection = ['task1', 'task2', 'task3'] # list
+collection = ["создать версию 1", "попить кофе"]
+print("Приветствуем вас в приложении  TASK MANAGER")
 
-print("Добро пожаловать!")
 while is_running:
-    print(" старт ")
-    print(collection)
-    is_running = False
-    print(" финиш ")
+    print('1 - посмотреть задачи'
+          '\n2 - добавить задачу'
+          '\n3 - редактировать задачу'
+          '\n4 - удалить задачу'
+          '\n5 - выход')
+    choice_user = input("введите команду: ")
 
-
-
-
-
+    match choice_user:
+        case "1":
+            print("-" * 30)
+            for key, item in enumerate(collection):
+                print(key + 1, item)
+            print("-" * 30)
+            waite = input("Нажмите 'ENTER' для продолжения")
+        case "2":
+            task_name = input('введите название задачи: ')
+            collection.append(task_name)
+        case "3":
+            for key, item in enumerate(collection):
+                print(key + 1, item)
+            select_edit = int(input('введите номер задачи: '))
+            edit_name = input("новое имя задачи: ")
+            collection[select_edit - 1] = edit_name
+        case "4":
+            for key, item in enumerate(collection):
+                print(key + 1, item)
+            delete_edit = int(input('введите номер задачи: '))
+            collection.pop(delete_edit - 1)
+        case "5":
+            print("отключение...")
+            is_running = False
+        case _:
+            print('неверная команда')
 
 
