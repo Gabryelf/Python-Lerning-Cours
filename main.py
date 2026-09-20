ddace290e7fb47a990a0ba826d5b7c3bc35160a0
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-09-20 16:10:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,116 @@
-"""
-    Точка входа в приложение Task Manager
-    version 0.0.4
-    --- description ---
-    проложение может сохранять задачи,
-    редактировать, выдает список задач
-    и может удалять задачу.
+""" Приложение Task Manager
+
+    =============================================================
+    консольное приложение - менеджер управления заметок,
+    пользователь может создать заметку, редактировать,
+    посмотреть все заметки или удалить выбранную.
+    =============================================================
+
+    version app 0.0.4
+    v(0.0.1)
+    разработан цикл приложения - структурное программирование
+
+    v(0.0.2)
+    внедрен  i/o функционал для ввода заметки
+
+    v(0.0.3)
+    разработаны функции для цикла - функциональное программирование
+
+    v(0.0.4)
+    добавлены проверки и подтверждения
 """
 
 is_running = True
-collection = ["task 1", "task 2"] # list
+collection = []
+print("Приветствуем вас в приложении  TASK MANAGER")
 
-print("Добро пожаловать!")
-while is_running:
-    print("1 - посмотреть задачи \n"
-          "2 - добавить задачу \n"
-          "3 - редактирование задачи \n"
-          "4 - удаление задачи \n"
-          "5 - выход")
 
-    choice_user = input("Введите свой выбор")
-    match str(choice_user):
-        case '1':
-            for i, j in enumerate(collection):
-                print(i + 1, j)
-        case '2':
-            add_task = input("Введите имя задачи для добавления")
-            collection.append(add_task)
-        case '3':
-            for i, j in enumerate(collection):
-                print(i + 1, j)
-            select_task = int(input("Введите номер задачи"))
-            edit_task = input("Введите новое имя задачи для редактирования")
-            collection[select_task - 1] = edit_task
-        case '4':
-            for i, j in enumerate(collection):
-                print(i + 1, j)
-            delete_task = int(input("Введите номер задачи для удаления"))
-            collection.pop(delete_task - 1)
-        case '5':
-            is_running = False
-            print("До свидания!")
-        case _:
-            print("Такого пункта нет!")
+def show_list(collection_list: list):
+    print("-" * 30)
+    for i, j in enumerate(collection_list):
+        print(i + 1, j.strip('\n'))
+    print("-" * 30)
+
+
+def check_confirm(check_funk: int):
+    check_task = int(input('введите номер задачи: '))
+    if (check_task <= len(collection)):
+        if(check_funk == 1):
+            print("Введите да/нет или yes/no")
+            confirm_edit = input(f'вы точно хотите редактировать задачу {check_task}')
+            if (confirm_edit.startswith('y') or confirm_edit.startswith('д')):
+                edit_name = input("новое имя задачи: ")
+                collection[check_task - 1] = edit_name
+                print(f"заметка {check_task} теперь называется {edit_name}")
+            else:
+                print("отмена редактирования!")
+        elif(check_funk == 2):
+            print("Введите да/нет или yes/no")
+            confirm_delete = input(f'вы точно хотите удалить задачу {check_task}')
+            if (confirm_delete.startswith('y') or confirm_delete.__str__().startswith('д')):
+                print(f"заметка {check_task} удалена")
+                collection.pop(check_task - 1)
+            else:
+                print("отмена удаления!")
+    else:
+        print(f"заметка {check_task} не найдена в списке")
+        print(f"всего{len(collection)} заметок в списке")
+
+
+def save_file(tasks):
+    print(tasks)
+    name = 'saves.txt'
+    file = open(name, 'w', encoding="utf-8")
+    for task in tasks:
+        file.write(f"{task}\n")
+    file.close()
+
+
+def load_file():
+    name = 'saves.txt'
+    file = open(name, 'r', encoding="utf-8")
+    tasks = []
+    for line in file:
+        print(line.strip('\n'))
+        tasks.append(line.strip('\r'))
+    print(tasks)
+    file.close()
+    return tasks
+
+
+def main(is_run):
+    while is_run:
+        print('1 - посмотреть задачи'
+              '\n2 - добавить задачу'
+              '\n3 - редактировать задачу'
+              '\n4 - удалить задачу'
+              '\n5 - выход')
+        choice_user = input("введите команду: ")
+
+        match choice_user:
+            case "1":
+                show_list(collection_list=collection)
+                input("Нажмите 'ENTER' для продолжения")
+            case "2":
+                task_name = input('введите название задачи: ')
+                collection.append(task_name)
+                save_file(collection)
+            case "3":
+                show_list(collection_list=collection)
+                check_confirm(1)    #редактируем = 1 - атрибут
+            case "4":
+                show_list(collection_list=collection)
+                check_confirm(2)    #удаляем = 2 - атрибут
+            case "5":
+                print("отключение...")
+                is_run = False
+            case _:
+                print('неверная команда')
+
+
+if __name__ == "__main__":
+    collection = load_file()
+    main(is_running)
+
+
+
